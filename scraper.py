c3d6550d057acd1dbd3e7c4b7da66d163751055a
Aymane Aghil <aghilaymane@gmail.com>
2023-05-13 18:50:08 +0200

    first commit

---
+++ scraper.py
@@ -0,0 +1,24 @@
+import requests
+
+
+ID_CREDENTIALS = ""
+PASS_CREDENTIALS = ""
+
+
+
+
+
+def scraper_from_UPC():
+    response = requests.get("https://api.fib.upc.edu/")
+
+    """
+    url = "https://sandboxdnac.cisco.com/api/system/v1/auth/token"
+
+    # API uses JSON.
+    headers = {'content-type': 'application/json'}
+    # Send HTTP POST with username and password.
+    response = requests.request("POST",
+                                url, 
+                                auth=HTTPBasicAuth(username, password),
+                                headers=headers, verify=False)
+        """
