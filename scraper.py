eb0dd3e979151da699096d3b4ca959f8be087554
Aymane Aghil <aghilaymane@gmail.com>
2023-05-14 02:26:08 +0200

    search first commit

--- scraper.py
+++ scraper.py
@@ -3,26 +3,25 @@ import json
 import elasticsearch
 import requests
 
-
 ID_CREDENTIALS = "rKORoexkrfgZYPI1ScyMg1kzd2u266szTPq9nbSA"
 PASS_CREDENTIALS = ""
 
 
 def scraper_from_UPC():
-    response = requests.get('https://api.fib.upc.edu/v2/assignatures/?format=json;lang=en;client_id='+ID_CREDENTIALS)
+    response = requests.get('https://api.fib.upc.edu/v2/assignatures/?format=json;lang=en;client_id=' + ID_CREDENTIALS)
     return response
 
 
 def get_courses(url_response):
     # print(url_response)
     courses = dict()
-    i=0
+    i = 0
     results = url_response.json()["results"]
     for result in results:
         course_url = result["guia"]
         if course_url is None:
             continue
-        content = course_url+';lang=en;client_id='+ID_CREDENTIALS
+        content = course_url + ';lang=en;client_id=' + ID_CREDENTIALS
         course_response = requests.get(content).json()
         course_id = course_response["id"]
         if course_id == "AC":
@@ -46,10 +45,11 @@ def get_courses(url_response):
         course_name = course_response["nom"]
         course_description = course_response["descripcio"]
 
-        courses[course_name] = {"course_id": course_id, "course_description": course_description, "professors": course_professors,
-                                "contents": course_contents, "objectives": course_objectives}
+        courses[course_name] = {"course_id": course_id, "course_description": course_description,
+                                "professors": course_professors.__str__(),
+                                "contents": course_contents.__str__(), "objectives": course_objectives.__str__()}
         print(i)
-        i+=1
+        i += 1
 
     return courses
 
@@ -57,6 +57,10 @@ def get_courses(url_response):
 def load_as_json(data):
     with open('data.json', 'w') as f:
         json.dump(data, f)
+
+
+def load_from_json(json_file):
+    return json.load(json_file)
 
 
 def read_password():
@@ -74,49 +78,76 @@ def connect_elastic():
 
 
 def create_elastic():
-
     client = connect_elastic()
 
     upc_mapping = {
-            "properties": {
-                "name": {"type": "text"},
-                "id": {"type": "keyword"},
-                "description": {"type": "text"},
-                "professors": {"type": "object"},
-                "contents": {"type": "object"},
-                "objectives": {"type": "object"}
-            }
+        "properties": {
+            "name": {"type": "text"},
+            "id": {"type": "keyword"},
+            "description": {"type": "text"},
+            "professors": {"type": "text"},
+            "contents": {"type": "text"},
+            "objectives": {"type": "text"}
+        }
     }
 
-    client.indices.create(index='upc_courses', mappings=upc_mapping)
+    client.indices.create(index='upc_courses3', mappings=upc_mapping)
 
     return client
 
 
-def load_to_es(client, courses, course_name):
-    course = courses[course_name]
-    course_id = course["course_id"]
-    course_description = course["course_description"]
-    course_profs = course["professors"]
-    course_content = course["contents"]
-    course_objectives = course["objectives"]
+def load_to_es(client, courses):
+    for course_name in courses.keys():
+        course = courses[course_name]
+        course_id = course["course_id"]
+        course_description = course["course_description"]
+        course_profs = course["professors"]
+        course_content = course["contents"]
+        course_objectives = course["objectives"]
 
-    doc = {
-        "name": course_name,
-        "id": course_id,
-        "description": course_description,
-        "professors": course_profs,
-        "contents": course_content,
-        "objectives": course_objectives,
+        doc = {
+            "name": course_name,
+            "id": course_id,
+            "description": course_description,
+            "professors": course_profs,
+            "contents": course_content,
+            "objectives": course_objectives,
 
+        }
+        client.index(index="upc_courses3", document=doc)
+
+
+def course_search(query_string):
+    client = connect_elastic()
+    query = {
+        "bool": {
+            "filter": {
+                "term": {
+                    "name": query_string
+                }
+            }
+        }
     }
-    client.index(index="upc_courses", document=doc)
+
+    query_s = {
+        "match": {
+                    "description": query_string
+        }
+    }
+    results = client.search(index="upc_courses3", query=query_s)
+    print(results)
+
 
 
 if __name__ == '__main__':
-    json_response = scraper_from_UPC()
-    upc_courses = get_courses(json_response)
-    load_as_json(upc_courses)
+    # scraper
+    #json_response = scraper_from_UPC()
+    #upc_courses = get_courses(json_response)
+    #load_as_json(upc_courses)
+
     # elasticsearch
-    clt = create_elastic()
-    load_to_es(clt, upc_courses, "Agriculture Culture")
+    # delete_index("upc_courses")
+    # clt = create_elastic()
+    # load_to_es(clt, upc_courses)
+
+    course_search("Algebra")
